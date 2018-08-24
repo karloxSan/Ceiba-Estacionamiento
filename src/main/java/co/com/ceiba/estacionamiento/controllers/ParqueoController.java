ae4faf0e9286ea3f22b50ee205cac5f741b6622b
karloxSan <42356968+karloxSan@users.noreply.github.com>
2018-08-24 07:20:32 -0500

    Ranombre servicios rest y configuracion de CrossOrigin

--- src/main/java/co/com/ceiba/estacionamiento/controllers/ParqueoController.java
+++ src/main/java/co/com/ceiba/estacionamiento/controllers/ParqueoController.java
@@ -3,39 +3,45 @@ package co.com.ceiba.estacionamiento.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.com.ceiba.estacionamiento.dtos.ParqueoEntradaDto;
 import co.com.ceiba.estacionamiento.services.IParqueoService;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
-@RequestMapping("/parqueo")
+@RequestMapping("/api")
 public class ParqueoController {
 
 	@Autowired
 	private IParqueoService parqueoService;
 
-	@GetMapping(value = "/listar_parqueados")
+	@GetMapping(value = "/parqueadero")
 	public List<ParqueoEntradaDto> listarParqueo() {
 		return parqueoService.findAllParqueados();
 	}
 
-	@PostMapping("/crear")
+	@PostMapping("/parqueadero")
+	@ResponseStatus(HttpStatus.CREATED)
 	public void crearParqueo(@RequestBody ParqueoEntradaDto parqueoEntradaDto) {
 		parqueoService.crear(parqueoEntradaDto);
 	}
 
-	@GetMapping(value = "/buscar/{placa}")
+	@GetMapping(value = "/parqueadero/{placa}")
 	public ParqueoEntradaDto buscarParqueo(@PathVariable String placa) {
 		return parqueoService.findByPlaca(placa);
 	}
 
-	@GetMapping(value = "/sacar/{placa}")
+	@PutMapping(value = "/parqueadero/{placa}")
 	public void actualizarParqueo(@PathVariable String placa) {
 		parqueoService.actualizarParqueo(placa);
 	}
